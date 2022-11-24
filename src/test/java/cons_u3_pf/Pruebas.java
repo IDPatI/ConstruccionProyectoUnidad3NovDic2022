2af189b88004de5ca3a8b5b028901ff1aa1596a4
Peopledbear52 <brauliosarmiento@outlook.com>
2022-11-24 16:41:05 -0600

    Hotfix, Pruebas Unitarias

--- src/test/java/cons_u3_pf/Pruebas.java
+++ src/test/java/cons_u3_pf/Pruebas.java
@@ -13,10 +13,11 @@ import org.junit.Test;
 import archivos.JsonParser;
 import archivos.LectorArchivos;
 import archivos.Validaciones;
+import modelo.Empleado;
 import modelo.Empleados;
 
 public class Pruebas {
-    private static final String rutaMia ="C:/Users/EQUIPO 1/Documents/prueba.json"; 
+    private static final String rutaMia ="D:/Desktop/archivo.json"; 
     @Test
     public void leerArchivo() throws Exception{
         String json = LectorArchivos.ObtenerContenido(rutaMia);
@@ -55,7 +56,16 @@ public class Pruebas {
     @Test
     public void pruebaModificarEmpleado() throws Exception{
         Empleados e  = new Empleados(rutaMia);
+        Empleado resultado = new Empleado();
+        resultado.firstName = "David Alberto";
+        resultado.lastName = "Pat Cituk";
+        resultado.photo = "https://cdn.discordapp.com/attachments/903704345747525737/1017236782292684900/DPat_A_beautiful_painting_of_a_big_purple_sci-fi_Gate_ea4dcb4d-aa2e-4476-b94c-df4156ad2d65.png";
+
+
+        
         e.modificarEmpleado("2","David Alberto", "Pat Cituk", "https://cdn.discordapp.com/attachments/903704345747525737/1017236782292684900/DPat_A_beautiful_painting_of_a_big_purple_sci-fi_Gate_ea4dcb4d-aa2e-4476-b94c-df4156ad2d65.png");
+        Empleado actual = Empleados.get("2");
+        assertEquals(resultado.toString(), actual.toString());
 
     }
 
